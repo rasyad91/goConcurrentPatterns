24a2889595a824968d94b08e51033c5ea5ffba8a
Rasyad <rasyadsubandrio@gmail.com>
2021-10-29 11:03:07 +0800

    a

--- 00-GoConcurrencyPatterns/03-fanIn/main.go
+++ 00-GoConcurrencyPatterns/03-fanIn/main.go
@@ -31,16 +31,8 @@ func boring(msg string) <-chan string {
 
 func fanIn(input1, input2 <-chan string) <-chan string {
 	c := make(chan string)
-	go func() {
-		for {
-			c <- <-input1
-		}
-	}()
-	go func() {
-		for {
-			c <- <-input2
-		}
-	}()
+	go func() { for{c <- <-input1}}()
+	go func() { for{c <- <-input2}}()
 
 	return c
 }
